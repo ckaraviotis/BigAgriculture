67fdaa22f8f24b17cfa0bad551ce83057e1eede6
Christian Karaviotis <christian.karaviotis@gmail.com>
2016-10-11 19:29:49 +0100

    Fix generator send/receive bug

--- src/main/java/solipsists/bigagriculture/tileentity/TileGenerator.java
+++ src/main/java/solipsists/bigagriculture/tileentity/TileGenerator.java
@@ -8,12 +8,17 @@ public class TileGenerator extends TileEnergyGeneric implements ITickable {
     private final static int transfer = 20;
 
     public TileGenerator() {
-        super(capacity, 0, transfer);
+        super(capacity, transfer, transfer);
     }
 
     @Override
 	public void update() {
         this.generate(transfer);
         this.push(transfer);
+    }
+
+    @Override
+    public boolean canReceive() {
+        return false;
     }
 }
